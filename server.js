4682f045aab9a15476704e5c09f6b6a54374666e
Surya Teja Reddy Mallidi <suryateja3614@gmail.com>
2020-04-23 21:47:58 -0700

    Connecting to MongoDB with Mongoose - 7

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect to MongoDB
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
