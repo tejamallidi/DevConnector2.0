9c183378b93734540e329dc1d457b1d5ad4a9894
Surya Teja Reddy Mallidi <suryateja3614@gmail.com>
2020-04-23 23:01:10 -0700

    Register User and request body validation - 9,10

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app = express();
 
 //Connect to MongoDB
 connectDB();
+
+// Init Middleware
+app.use(express.json({
+    extended: false
+}))
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
@@ -14,4 +20,5 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
