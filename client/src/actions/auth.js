a2ea4c174c492c9537e0e5d0ef4d3a40a583a3b9
Surya Teja Reddy Mallidi <suryateja3614@gmail.com>
2020-04-30 12:45:42 -0700

    Profile Reducer and get current profile, Starting on the dashboard - 45,46

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -80,6 +81,9 @@ export const login = ({ email, password }) => async (dispatch) => {
 
 // Logout
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to='/profiles'>Developers</Link>
+        <Link to='#!'>Developers</Link>
       </li>
       <li>
         <Link to='/posts'>Posts</Link>
