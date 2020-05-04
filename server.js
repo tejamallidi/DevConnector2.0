b3123f16c9654bed009488265d9ce0c2280e9048
Surya Teja Reddy Mallidi <suryateja3614@gmail.com>
2020-05-04 16:38:07 -0700

    Changes for deployment and add not found page.

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const connectDB = require('./config/db');
-
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 //Connect to MongoDB
 connectDB();
 
 // Init Middleware
-app.use(express.json({
-    extended: false
-}))
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
 // Define Routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set Static Folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
